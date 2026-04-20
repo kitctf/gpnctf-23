341ca8443fa51d5d24ce09c89e47cecdfd7f4259
intrigus <abc123zeus@live.de>
2026-04-20 02:21:49 +0200

    Update backend urls

---
+++ _app/immutable/chunks/Bgpf04He.js
@@ -0,0 +1,1 @@
+const t="GPN CTF 2025",C="https://gpn23.ctf.kitctf.de/api",s="/",L="GPNCTF{…}";export{t as P,s as a,C as b,L as c};
---
+++ _app/immutable/nodes/12.BskJ2LgD.js
@@ -0,0 +1,1 @@
+import{r as e}from"../chunks/DJ4wJY-I.js";const o=()=>{e()},t=Object.freeze(Object.defineProperty({__proto__:null,load:o},Symbol.toStringTag,{value:"Module"}));export{t as universal};
